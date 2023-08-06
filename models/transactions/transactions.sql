d1501da5842224895ad1c395dc8d28651f7a2155
hugo.ruiz.verastegui@gmail.com <hugo.ruiz.verastegui@gmail.com>
2023-08-06 10:11:21 +0000

    added tests

--- models/transactions/transactions.sql
+++ models/transactions/transactions.sql
@@ -1,7 +1,7 @@
 select 
 replace(`Account No`,"'","") as account_no, 
 `DATE` as date, 
-`TRANSACTION DETAILS` as transactions_details,
+`TRANSACTION DETAILS` as transaction_details,
 `CHQ_NO` as chq_no,
 `VALUE DATE` as value_date,
 ` WITHDRAWAL AMT ` as withdrawal_amt,
