8c9e34053abd42e9a3e8246bcfb62305b0d9ef35
hugo.ruiz.verastegui@gmail.com <hugo.ruiz.verastegui@gmail.com>
2023-08-06 12:29:41 +0000

    update data types

--- models/transactions/transactions.sql
+++ models/transactions/transactions.sql
@@ -1,11 +1,11 @@
 select 
-replace(`Account No`,"'","") as account_no, 
+CAST(replace(`Account No`,"'","") AS INTEGER) as account_no, 
 DATE_ADD(PARSE_DATE('%b %d, %Y',`DATE`), INTERVAL 2000 YEAR) as date,
 `TRANSACTION DETAILS` as transaction_details,
-`CHQNO` as chq_no,
+CAST(`CHQNO` AS INTEGER) as chq_no,
 DATE_ADD(PARSE_DATE('%b %d, %Y',`VALUE DATE`), INTERVAL 2000 YEAR) as value_date,
-` WITHDRAWAL AMT ` as withdrawal_amt,
-` DEPOSIT AMT ` as deposit_amt,
-`BALANCE AMT` as balance_amt
+CAST(` WITHDRAWAL AMT ` AS FLOAT64) as withdrawal_amt,
+CAST(` DEPOSIT AMT ` AS FLOAT64) as deposit_amt,
+CAST(`BALANCE AMT` AS FLOAT64) as balance_amt
 
 from `dbt-test-395011.dbt_production.transactions_data`
