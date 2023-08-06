f214ebcda38108d6a7a893bd3214710b16c49151
hugo.ruiz.verastegui@gmail.com <hugo.ruiz.verastegui@gmail.com>
2023-08-06 12:15:13 +0000

    improved dates in model

--- models/transactions/transactions.sql
+++ models/transactions/transactions.sql
@@ -1,9 +1,9 @@
 select 
 replace(`Account No`,"'","") as account_no, 
-`DATE` as date, 
+DATE_ADD(PARSE_DATE('%b %d, %Y',`DATE`), INTERVAL 2000 YEAR) as date,
 `TRANSACTION DETAILS` as transaction_details,
 `CHQNO` as chq_no,
-`VALUE DATE` as value_date,
+DATE_ADD(PARSE_DATE('%b %d, %Y',`VALUE DATE`), INTERVAL 2000 YEAR) as value_date,
 ` WITHDRAWAL AMT ` as withdrawal_amt,
 ` DEPOSIT AMT ` as deposit_amt,
 `BALANCE AMT` as balance_amt
