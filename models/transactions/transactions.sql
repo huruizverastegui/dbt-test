63d788d4547a7413caefccc679cdbc62c7b545ee
hugo.ruiz.verastegui@gmail.com <hugo.ruiz.verastegui@gmail.com>
2023-08-06 07:37:50 +0000

    move to transactions data

---
+++ models/transactions/transactions.sql
@@ -0,0 +1,11 @@
+select 
+replace(`Account No`,"'","") as account_no, 
+`DATE` as date, 
+`TRANSACTION DETAILS` as transactions_details,
+`CHQ_NO` as chq_no,
+`VALUE DATE` as value_date,
+` WITHDRAWAL AMT ` as withdrawal_amt,
+` DEPOSIT AMT ` as deposit_amt,
+`BALANCE AMT` as balance_amt
+
+from `dbt-test-395011.dbt_production.transactions_data`
