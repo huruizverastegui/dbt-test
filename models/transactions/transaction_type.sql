f07c3e1ce3a9f7d79d4f9facf9e3ec3a8a1dd539
hugo.ruiz.verastegui@gmail.com <hugo.ruiz.verastegui@gmail.com>
2023-08-08 11:47:22 +0000

    transaction type final

--- models/transactions/transaction_type.sql
+++ models/transactions/transaction_type.sql
@@ -3,5 +3,13 @@ transaction_id,
 transaction_details, 
 CASE 
 WHEN transaction_details like '%Indiaforensic%' THEN 'Indiaforensic'
-END 
+WHEN transaction_details like '%CASHDEP%' THEN 'Cash deposit'
+WHEN transaction_details like '%RTGS%' THEN 'Real-time Gross Settlement'
+WHEN transaction_details like '%IMPS%' THEN 'Immediate Payment Service'
+WHEN transaction_details like '%RUPAY%' THEN 'RUPAY'
+WHEN transaction_details like '%AEPS%' THEN 'Aadhaar Enabled Payment System'
+WHEN transaction_details like '%INTERNAL FUND%' THEN 'Internal fund transfer'
+ELSE 'Others'
+
+END  as transaction_type
 from `dbt-test-395011.dbt_huruizverastegui.transactions`
