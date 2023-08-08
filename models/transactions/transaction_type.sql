ca5fdb96de85f6215609204a0672f21baba5c262
hugo.ruiz.verastegui@gmail.com <hugo.ruiz.verastegui@gmail.com>
2023-08-08 11:35:57 +0000

    add transaction type

--- models/transactions/transaction_type.sql
+++ models/transactions/transaction_type.sql
@@ -1,11 +1,5 @@
 select 
-CAST(replace(`Account No`,"'","") AS INTEGER) as account_no, 
-DATE_ADD(PARSE_DATE('%b %d, %Y',`DATE`), INTERVAL 2000 YEAR) as date,
-`TRANSACTION DETAILS` as transaction_details,
-CAST(`CHQNO` AS INTEGER) as chq_no,
-DATE_ADD(PARSE_DATE('%b %d, %Y',`VALUE DATE`), INTERVAL 2000 YEAR) as value_date,
-CAST(` WITHDRAWAL AMT ` AS FLOAT64) as withdrawal_amt,
-CAST(` DEPOSIT AMT ` AS FLOAT64) as deposit_amt,
-CAST(`BALANCE AMT` AS FLOAT64) as balance_amt
+transaction_id,
+transaction_details
 
-from `dbt-test-395011.dbt_huruizverastegui.transactions_data`
+from `dbt-test-395011.dbt_huruizverastegui.transactions`
